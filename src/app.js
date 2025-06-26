644c8c1aa20555c7479243b9c63c2ada44c58e89
Anderson Gonçalves <anderson.dev93@gmail.com>
2025-06-26 13:02:37 -0300

    novo ajuste no app.js

--- src/app.js
+++ src/app.js
@@ -5,23 +5,16 @@ import authRoutes from './routes/authRoutes.js';
 import adminRoutes from './routes/adminRoutes.js';
 import consultaRoutes from './routes/consultaRoutes.js';
 import usuarioRoutes from './routes/usuarioRoutes.js';
-import cookieParser from 'cookie-parser';
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use(cookieParser()); 
+
 app.use(cors({
-  origin: 'http://localhost:3000', 
-  credentials: true,                 
+  origin: 'http://localhost:3000',                 
 }));
 
-// Exemplo middleware que verifica cookie/sessão
-app.use((req, res, next) => {
-  console.log('Cookies:', req.cookies); // Veja se cookie aparece
-  next();
-});
 
 app.use('/api/auth', authRoutes);
 app.use('/api/admin', adminRoutes);
